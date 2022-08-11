89a6d5eb6f6a8c5498e65a1d1a06209ced63c655
Trofimov Alexandr <darax111@mail.ru>
2022-08-11 11:11:17 +0400

    first commit

---
+++ src/main/java/com/petproject/jwtapptest/JwtAppTestApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.jwtapptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAppTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAppTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petproject/jwtapptest/JwtAppTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petproject.jwtapptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAppTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
